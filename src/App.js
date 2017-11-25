70d24467e4e6c1997ce01a24edd841a4987cb3f6
FinnVoid <neku113@gmail.com>
2017-11-24 19:40:28 -0700

    Now Fetchs Book Data from API, scrapped old data and residual code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import React, {Component} from 'react'
+import {Route} from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 import ListBooks from './components/ListBooks'
 import SearchPage from './components/SearchPage'
 
-class BooksApp extends React.Component {
-  state = {}
+class BooksApp extends Component {
+  state = {
+    books: []
+  }
+
+  shelfs = [
+
+    {
+      name: 'currentlyReading',
+      heading: 'Currently Reading'
+    }, {
+      name: 'wantToRead',
+      heading: "Want to Read"
+    }, {
+      name: 'read',
+      heading: 'Read'
+    }
+  ]
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => this.setState({books}))
+  }
 
   render() {
     return (<div className="app">
       <Route exact="exact" path="/search" render={() => (<SearchPage/>)}/>
 
-      <Route exact="exact" path="/" render={() => (<ListBooks/>)}/>
-      </div>
-      )
+      <Route exact="exact" path="/" render={() => (<ListBooks shelfs={this.shelfs} books={this.state.books}/>)}/>
+    </div>)
   }
 }
 
